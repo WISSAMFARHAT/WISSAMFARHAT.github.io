d9ce769aab93740b739b8ba41f92d70266e8d626
Wissam Farhat <WissamFarhat@angrymonkeyagency.com>
2023-07-05 22:49:40 +0300

    final update

--- animation.js
+++ animation.js
@@ -2,10 +2,18 @@ const fadersleft = document.querySelectorAll(".fade-left");
 const fadersright = document.querySelectorAll(".fade-right");
 const faderin = document.querySelectorAll(".fadein");
 const faderout = document.querySelectorAll(".fadeout");
+const item = document.querySelectorAll(".resume-grid-item");
+const percentage = document.querySelectorAll(".percentage");
 
 const appearOptions = {
   threshold: 0,
   rootMargin: "0px 0px -100px 0px"
+};
+
+
+const appearOptions1 = {
+  threshold: 0,
+  rootMargin: "0px 0px -40px 0px"
 };
 
 const appearScroll = new IntersectionObserver(function (entries) {
@@ -18,6 +26,17 @@ const appearScroll = new IntersectionObserver(function (entries) {
     }
   });
 }, appearOptions);
+
+const appearScroll1 = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("_appear");
+      //appearScroll.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("_appear");
+    }
+  });
+}, appearOptions1);
 
 if (fadersleft) {
   fadersleft.forEach(function (fader) {
@@ -40,5 +59,17 @@ if (faderin) {
 if (faderout) {
   faderout.forEach(function (fader) {
     appearScroll.observe(fader);
+  });
+}
+
+if (item) {
+  item.forEach(function (fader) {
+    appearScroll1.observe(fader);
+  });
+}
+
+if (percentage) {
+  percentage.forEach(function (fader) {
+    appearScroll1.observe(fader);
   });
 }
--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const mobileMenu=document.querySelector(".header-mobile");
 const closeMenu=document.querySelector(".popup-close");
 const menuContent=document.querySelector(".popup-content");
-
+const theme=document.querySelector("#theme");
+const closer=document.querySelector("#close");
+const next=document.querySelector(".next");
+const prev=document.querySelector(".prev");
+const items=document.querySelectorAll(".item");
+const box=document.querySelector(".color_box");
+const colors=document.querySelectorAll(".colorhex");
+const ages=document.querySelector(".age");
+const send=document.querySelector("form");
+const currentYear = new Date().getFullYear();
+let minus1998 = currentYear - 1998;
+ages.innerHTML=minus1998+" Years";
 
 mobileMenu.addEventListener("click",function(){
     document.querySelector("html").classList.add("_open");
@@ -32,4 +43,75 @@ function checkScrollHeight() {
     }
 }
 
+theme.addEventListener("click",function(event){
+    box.classList.toggle("_opened");
+});
+
+closer.addEventListener("click",function(e){
+    box.classList.remove("_opened");
+});
+
+
+next.addEventListener("click",function(event){
+    var currentpage=document.querySelector(".currentpage").innerHTML;
+    ChangePage(currentpage);
+});
+
+prev.addEventListener("click",function(event){
+    var currentpage=document.querySelector(".currentpage").innerHTML;
+    ChangePage(currentpage);
+});
+
+function ChangePage(pagenumber){
+    if(pagenumber==1)
+    {
+        document.querySelector(".currentpage").innerHTML=2;
+        items.forEach(item=>item.classList.remove("disabled"));
+        items.forEach(item=>item.classList.add("disabled"));
+        items[3].classList.remove("disabled");
+        items[4].classList.remove("disabled");
+        items[5].classList.remove("disabled");
+
+    
+    }else if(pagenumber==2)
+    {
+        document.querySelector(".currentpage").innerHTML=3;
+        items.forEach(item=>item.classList.remove("disabled"));
+        items.forEach(item=>item.classList.add("disabled"));       
+         items[6].classList.remove("disabled");
+        items[7].classList.remove("disabled");
+        items[8].classList.remove("disabled");
+    }else if(pagenumber==3)
+    {
+        document.querySelector(".currentpage").innerHTML=4;
+        items.forEach(item=>item.classList.remove("disabled"));
+        items.forEach(item=>item.classList.add("disabled")); 
+        items[9].classList.remove("disabled");
+        items[10].classList.remove("disabled");
+        items[11].classList.remove("disabled");
+    }else if(pagenumber==4)
+    {
+        document.querySelector(".currentpage").innerHTML=5;
+        items.forEach(item=>item.classList.remove("disabled"));
+        items.forEach(item=>item.classList.add("disabled")); 
+        items[12].classList.remove("disabled");
+        items[13].classList.remove("disabled");
+        items[14].classList.remove("disabled");
+    }else
+    {
+        document.querySelector(".currentpage").innerHTML=1;
+        items.forEach(item=>item.classList.remove("disabled"));
+        items.forEach(item=>item.classList.add("disabled")); 
+        items[1].classList.remove("disabled");
+        items[2].classList.remove("disabled");
+        items[3].classList.remove("disabled");
+    }
+}
+
+
+colors.forEach(colorindex=>{
+    colorindex.addEventListener("click",function(){
+        document.documentElement.style.setProperty("--primaryColor", colorindex.getAttribute("data-color"));
+    })
+})
 
